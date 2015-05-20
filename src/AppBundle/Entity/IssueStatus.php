3621e093fd03971ea54947230d810fd5365539c4
Vladimir Gavrylov <iboved@gmail.com>
2015-05-20 12:55:57 +0300

    Removed unnecessary Assert annotation

--- src/AppBundle/Entity/IssueStatus.php
+++ src/AppBundle/Entity/IssueStatus.php
@@ -3,7 +3,6 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
 use JMS\Serializer\Annotation\Type;
 
 /**
@@ -27,7 +26,6 @@ class IssueStatus
      * @var string
      *
      * @ORM\Column(name="name", type="string", length=255)
-     * @Assert\NotBlank()
      * @Type("string")
      */
     private $name;
