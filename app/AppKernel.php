113b3cc1212696b6ba79d1cb60e6a2fb75ac973c
Vladimir Gavrylov <iboved@gmail.com>
2015-05-21 11:41:33 +0300

    Added FOSRestBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Misd\GuzzleBundle\MisdGuzzleBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
