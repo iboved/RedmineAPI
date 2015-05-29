c798ccb256dba9759a7f816c34c21155e62445fb
Vladimir Gavrylov <iboved@gmail.com>
2015-05-29 09:55:15 +0300

    Added getAction in ProjectController

--- src/AppBundle/Controller/ProjectController.php
+++ src/AppBundle/Controller/ProjectController.php
@@ -8,6 +8,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use FOS\RestBundle\Controller\Annotations\QueryParam;
 use FOS\RestBundle\Request\ParamFetcher;
 use AppBundle\Model\ProjectsDTO;
+use AppBundle\Model\ProjectDTO;
 
 class ProjectController extends Controller
 {
@@ -38,6 +39,34 @@ class ProjectController extends Controller
 
         return $this->render('project/get_all.html.twig', [
             'projects' => $projectsDTO->projects
+        ]);
+    }
+
+    /**
+     * Returns one project by id
+     *
+     * @QueryParam(name="include", description="Fetch associated data")
+     *
+     * @Route("/projects/{id}")
+     * @Method({"GET"})
+     *
+     * @param ParamFetcher $paramFetcher
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function getAction(ParamFetcher $paramFetcher, $id)
+    {
+        $client = $this->get('redmine.client');
+        $command = $client->getCommand('GetProject', [
+            'id' => $id,
+            'include' => $paramFetcher->get('include')
+        ]);
+
+        /** @var ProjectDTO $projectDTO */
+        $projectDTO = $client->execute($command);
+
+        return $this->render('project/get.html.twig', [
+            'projects' => $projectDTO->project
         ]);
     }
 }
